2f994b874f81170cfed9f643c46c37a3d4929ec9
PizzaG <49663808+PizzaG@users.noreply.github.com>
2021-10-05 15:07:44 -0500

    Revert "Uneeded"
    This reverts commit f09d0b443c3e3718f3d15796ae9e0e8612d05a3f.

---
+++ vendorsetup.sh
@@ -0,0 +1,9 @@
+#
+# Copyright (C) 2020 The Android Open Source Project
+# Copyright (C) 2020 The TWRP Open Source Project
+# Copyright (C) 2020 SebaUbuntu's TWRP device tree generator
+# Copyright (C) 2019-Present A-Team Digital Solutions
+#
+
+add_lunch_combo omni_gtelwifiue-userdebug
+add_lunch_combo omni_gtelwifiue-eng
