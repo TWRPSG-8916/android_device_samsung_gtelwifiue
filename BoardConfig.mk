67c1d347b1e25fa9843d107d6c6c49ae5e7e5077
DJABhipHop <djabhiphop-DJABHipHop@yahoo.com>
2021-10-17 12:13:49 -0400

    Remove TW_INPUT_BLACKLIST

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -82,7 +82,6 @@ TW_EXCLUDE_TWRPAPP := true
 TW_BRIGHTNESS_PATH := "/sys/class/leds/lcd-backlight/brightness"
 TW_USE_NEW_MINADBD := true
 TW_INCLUDE_NTFS_3G := true
-TW_INPUT_BLACKLIST := "accelerometer\x0ahbtp_vm"
 TW_MAX_BRIGHTNESS := 255
 TW_DEFAULT_BRIGHTNESS := 150
 TW_NEW_ION_HEAP := true
